96e027e33a963375d5b128e3694f2ac86b25bc7b
Jonny <jonathaneichhain@gmx.de>
2021-10-01 03:19:22 +0200

    Added support for tools, if the tool wouldnt give a drop standard break is used. Fixed bugs Satisfied lazer

--- src/main/java/GameListener.java
+++ src/main/java/GameListener.java
@@ -16,6 +16,7 @@ import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.material.MaterialData;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -36,10 +37,10 @@ public class GameListener implements Listener {
     @EventHandler
     private void onDamageBlock(BlockDamageEvent e) {
 
-        if(!CommandInstamine.isIsInastamining(e.getPlayer())){
+        if (!CommandInstamine.isIsInastamining(e.getPlayer())) {
             return;
         }
-        if(e.getBlock().getType() == Material.BEDROCK){
+        if (e.getBlock().getType() == Material.BEDROCK) {
             return;
         }
 
@@ -48,145 +49,77 @@ public class GameListener implements Listener {
 
             Player player = e.getPlayer();
             Location loc = e.getBlock().getLocation();
-            HashMap<Integer, ? extends ItemStack> isInInv = new HashMap<Integer, ItemStack>();
+            HashMap<Integer, ? extends ItemStack> isInInv;
             boolean addedSuccess = false;
-            if (player.getInventory().firstEmpty() == -1) {         //Is inventory full
-                if (e.getPlayer().getInventory().contains(e.getBlock().getType())) {
+            if (e.getBlock().getBlockData() instanceof Bisected) {
+                Bisected b = ((Bisected) e.getBlock().getBlockData());
+                if (b.getHalf() == Bisected.Half.TOP) {
+                    loc.add(0, -1, 0);
+                }
 
-                    isInInv = e.getPlayer().getInventory().all(e.getBlock().getType());
-                    for (Map.Entry<Integer, ? extends ItemStack> entry : isInInv.entrySet())
+            }
 
-                        if (entry.getValue().getAmount() < entry.getValue().getType().getMaxStackSize()) {
-                            if(e.getBlock().getBlockData() instanceof SeaPickle) {
-                                SeaPickle s = ((SeaPickle) e.getBlock().getBlockData());
-                                int newAmmount = entry.getValue().getAmount() + s.getPickles();
-                                entry.getValue().setAmount(newAmmount);
-                                e.getBlock().setType(Material.AIR);
-                                addedSuccess = true;
-                            }
-                            else if(e.getBlock().getBlockData() instanceof TurtleEgg) {
-                                TurtleEgg t = ((TurtleEgg) e.getBlock().getBlockData());
-                                int newAmmount = entry.getValue().getAmount() + t.getEggs();
-                                entry.getValue().setAmount(newAmmount);
-                                e.getBlock().setType(Material.AIR);
-                                addedSuccess = true;
-                            }
-                            else if (e.getBlock().getBlockData() instanceof Bisected) {
+            Collection<ItemStack> collection = loc.getBlock().getDrops(e.getItemInHand(),e.getPlayer());
+            if (collection.size() == 0) {
+                collection = loc.getBlock().getDrops();
+            }
+            if (collection.size() == 0){
+                loc.getBlock().setType(Material.AIR);
+            }
+            else {
+                if (player.getInventory().firstEmpty() == -1) { //Is inventory full
+                    for (ItemStack itemStack : collection) {
+                        if (e.getPlayer().getInventory().contains(itemStack.getType())) {
+                            isInInv = e.getPlayer().getInventory().all(itemStack.getType());
+                            for (Map.Entry<Integer, ? extends ItemStack> entry : isInInv.entrySet()) {
+                                if (entry.getValue().getAmount() < entry.getValue().getType().getMaxStackSize()) {
 
-                                Bisected b = ((Bisected) e.getBlock().getBlockData());
-                                if (b.getHalf().equals(Bisected.Half.TOP)) {
-                                    e.getBlock().getLocation().add(0, -1, 0).getBlock().setType(Material.AIR);
-
-                                    int newAmmount = entry.getValue().getAmount() + 1;
+                                    int newAmmount = entry.getValue().getAmount() + itemStack.getAmount();
                                     entry.getValue().setAmount(newAmmount);
-                                    e.getBlock().setType(Material.AIR);
+                                    loc.getBlock().setType(Material.AIR);
+
                                     addedSuccess = true;
-
-
-
+                                    break;
                                 }
 
-                            }else {
-                                int newAmmount = entry.getValue().getAmount() + 1;
-                                entry.getValue().setAmount(newAmmount);
-                                e.getBlock().setType(Material.AIR);
-
-                                addedSuccess = true;
-                                break;
                             }
                         }
-
-                }
-            }
-            else if(e.getBlock().getState() instanceof ShulkerBox){
-                ShulkerBox s = ((ShulkerBox) e.getBlock().getState());
-                ItemStack shulker = new ItemStack(e.getBlock().getType());
-                BlockStateMeta bsm = (BlockStateMeta) shulker.getItemMeta();
-                bsm.setBlockState(s);
-                shulker.setItemMeta(bsm);
-
-                e.getPlayer().getInventory().addItem(shulker);
-                e.getBlock().setType(Material.AIR);
-                addedSuccess = true;
-
-            }
-            else if(e.getBlock().getBlockData() instanceof SeaPickle){
-                SeaPickle s = ((SeaPickle) e.getBlock().getBlockData());
-                ItemStack newItemStack = new ItemStack(e.getBlock().getType());
-                newItemStack.setAmount(s.getPickles());
-                e.getPlayer().getInventory().addItem(newItemStack);
-                e.getBlock().setType(Material.AIR);
-                addedSuccess = true;
-
-            }
-            else if(e.getBlock().getBlockData() instanceof TurtleEgg){
-                TurtleEgg t = ((TurtleEgg) e.getBlock().getBlockData());
-                ItemStack newItemStack = new ItemStack(e.getBlock().getType());
-                newItemStack.setAmount(t.getEggs());
-                e.getPlayer().getInventory().addItem(newItemStack);
-                e.getBlock().setType(Material.AIR);
-                addedSuccess = true;
-
-            }
-
-
-
-            else if (e.getBlock().getBlockData() instanceof Bisected) {
-
-                Bisected b = ((Bisected) e.getBlock().getBlockData());
-                if (b.getHalf().equals(Bisected.Half.TOP)) {
-
-
-                    ItemStack newItemStack = new ItemStack(e.getBlock().getType());
-                    e.getPlayer().getInventory().addItem(newItemStack);
-                    e.getBlock().setType(Material.AIR);
-                    addedSuccess = true;
-                    e.getBlock().getLocation().add(0, -1, 0).getBlock().setType(Material.AIR);
-
-
-                }
-
-            } else if (e.getBlock().getBlockData() instanceof Bed) {
-
-                Bed b = ((Bed) e.getBlock().getBlockData());
-                Bukkit.getLogger().info(b.getFacing().name());
-                if (b.getPart() == Bed.Part.FOOT) {
-                    String facing = b.getFacing().name();
-
-
-                    ItemStack newItemStack = new ItemStack(e.getBlock().getType());
-                    e.getPlayer().getInventory().addItem(newItemStack);
-                    e.getBlock().setType(Material.AIR, false);
-                    addedSuccess = true;
-                    if (facing.equals("NORTH")) {
-                        e.getBlock().getLocation().add(0, 0, -1).getBlock().setType(Material.AIR);
-                    } else if (facing.equals("SOUTH")) {
-                        e.getBlock().getLocation().add(0, 0, 1).getBlock().setType(Material.AIR);
-                    } else if (facing.equals("WEST")) {
-                        e.getBlock().getLocation().add(-1, 0, 0).getBlock().setType(Material.AIR);
-                    } else if (facing.equals("EAST")) {
-                        e.getBlock().getLocation().add(1, 0, 0).getBlock().setType(Material.AIR);
                     }
-                } else {
-                    ItemStack newItemStack = new ItemStack(e.getBlock().getType());
+                }
+                else {
+                    for (ItemStack itemStack : collection) {
+                        if (e.getBlock().getBlockData() instanceof Bed) {
 
-                    e.getPlayer().getInventory().addItem(newItemStack);
-                    e.getBlock().setType(Material.AIR);
-                    addedSuccess = true;
+                            Bed b = ((Bed) e.getBlock().getBlockData());
+                            if (b.getPart() == Bed.Part.FOOT) {
+                                String facing = b.getFacing().name();
+                                e.getPlayer().getInventory().addItem(itemStack);
+                                e.getBlock().setType(Material.AIR, false);
+                                addedSuccess = true;
+                                if (facing.equals("NORTH")) {
+                                    e.getBlock().getLocation().add(0, 0, -1).getBlock().setType(Material.AIR);
+                                } else if (facing.equals("SOUTH")) {
+                                    e.getBlock().getLocation().add(0, 0, 1).getBlock().setType(Material.AIR);
+                                } else if (facing.equals("WEST")) {
+                                    e.getBlock().getLocation().add(-1, 0, 0).getBlock().setType(Material.AIR);
+                                } else if (facing.equals("EAST")) {
+                                    e.getBlock().getLocation().add(1, 0, 0).getBlock().setType(Material.AIR);
+                                }
+                            }
+                        } else {
+                            e.getPlayer().getInventory().addItem(itemStack);
+                            loc.getBlock().setType(Material.AIR);
+                            addedSuccess = true;
+                        }
+                    }
                 }
 
-            } else {
-                ItemStack newItemStack = new ItemStack(e.getBlock().getType());
-                e.getPlayer().getInventory().addItem(newItemStack);
-                e.getBlock().setType(Material.AIR);
-                addedSuccess = true;
+                e.setCancelled(true);
+                if (!addedSuccess) {
+                    e.getPlayer().sendMessage("You dont have space in your inventory");
+                }
             }
 
-
-            e.setCancelled(true);
-            if (!addedSuccess) {
-                e.getPlayer().sendMessage("You dont have space in your inventory");
-            }
         }
     }
 }
